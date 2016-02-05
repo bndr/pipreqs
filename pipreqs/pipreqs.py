bb1eb59a7b01c6658e9675e86312c9daf54bdcd1
nrhine1 <nrhineha@cs.cmu.edu>
2016-02-05 11:18:08 -0500

    comma separated dirs

--- pipreqs/pipreqs.py
+++ pipreqs/pipreqs.py
@@ -13,7 +13,7 @@ Options:
                           $ export HTTP_PROXY="http://10.10.1.10:3128"
                           $ export HTTPS_PROXY="https://10.10.1.10:1080"
     --debug               Print debug information
-    --ignore <dirs>...    Ignore extra directories         
+    --ignore <dirs>       Ignore extra directories, each separated by a comma
     --encoding <charset>  Use encoding parameter for file open
     --savepath <file>     Save the list of requirements in the given file
     --force               Overwrite existing requirements.txt
@@ -50,7 +50,12 @@ def get_all_imports(path, encoding=None, extra_ignore_dirs = []):
     candidates = []
     ignore_errors = False
     ignore_dirs = [".hg", ".svn", ".git", "__pycache__", "env", "venv"]
-    ignore_dirs.extend(extra_ignore_dirs)
+
+    e_ignore_dirs_parsed = []
+    for e in extra_ignore_dirs:
+        e_s = os.path.basename(os.path.realpath(e))
+        e_ignore_dirs_parsed.append(e_s)
+    ignore_dirs.extend(e_ignore_dirs_parsed)
 
     for root, dirs, files in os.walk(path):
         dirs[:] = [d for d in dirs if d not in ignore_dirs]
@@ -195,7 +200,7 @@ def join(f):
 
 def init(args):
     encoding = args.get('--encoding')
-    extra_ignore_dirs = args.get('--ignore', []) 
+    extra_ignore_dirs = args.get('--ignore').split(',')
 
     candidates = get_all_imports(args['<path>'], 
                                  encoding=encoding,
