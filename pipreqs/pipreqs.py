fd08b3e5b3fea196ae5460a0c2892b9ade56efb0
nrhine1 <nrhineha@cs.cmu.edu>
2016-02-05 11:54:48 -0500

    None guard in ignore arg parsing

--- pipreqs/pipreqs.py
+++ pipreqs/pipreqs.py
@@ -200,7 +200,10 @@ def join(f):
 
 def init(args):
     encoding = args.get('--encoding')
-    extra_ignore_dirs = args.get('--ignore').split(',')
+    extra_ignore_dirs = args.get('--ignore', [])
+
+    if isinstance(extra_ignore_dirs, list):
+        extra_ignore_dirs = extra_ignore_dirs.split(',')
 
     candidates = get_all_imports(args['<path>'], 
                                  encoding=encoding,
